55137b6583499c7cc32929e03f37693839947eae
ManPuyol <72559693+ManPuyol@users.noreply.github.com>
2024-03-20 15:52:01 +0000

    Remove unused SQL files and update user schema

--- drizzle/0001_material_solo.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" serial PRIMARY KEY NOT NULL
-);
--- schemas/user.ts
+++ schemas/user.ts
@@ -1,8 +1,10 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, varchar, pgEnum } from "drizzle-orm/pg-core";
+
+export const roleEnum = pgEnum('role', ['teacher', 'student', 'admin']);
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
-  // fullName: text('full_name'),
-  // phone: varchar('phone', { length: 256 }),
+  fullName: text('full_name'),
+  email: varchar('email', { length: 256 }),
+  role: roleEnum('role'),
 });
-        
