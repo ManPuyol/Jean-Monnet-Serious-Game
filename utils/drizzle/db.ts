2b3ef1ac879030cafbca611a4e4a23c67be199d8
ManPuyol <72559693+ManPuyol@users.noreply.github.com>
2024-03-21 14:01:22 +0000

    added DB configurations

---
+++ utils/drizzle/db.ts
@@ -0,0 +1,11 @@
+
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+
+const connectionString = process.env.DATABASE_URL!
+
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(connectionString)
+
+export const db = drizzle(client);
+        
