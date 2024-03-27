8a3f75a26a3e84f95264d45d64899d90c1688a84
Manuel Ortega <maopmore@gmail.com>
2024-03-27 18:48:16 +0100

    Delete unused SQL and JSON files, and add a new column to the units table

--- schemas/units.ts
+++ schemas/units.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, boolean, timestamp, integer, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, boolean, timestamp, integer, varchar, smallint} from "drizzle-orm/pg-core";
 import { subjects } from "./subjects";
 
 export const units = pgTable('units', {
@@ -6,6 +6,7 @@ export const units = pgTable('units', {
   name: varchar('description', { length: 256 }),
   subjectId: integer('subject_id').references(() => subjects.id),
   active: boolean('active').default(true),
+  questions_per_quiz: smallint('questions_per_quiz').default(10),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
