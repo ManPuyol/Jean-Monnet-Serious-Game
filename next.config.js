99faaa6bf229f6be5ff517e09e05c084e446c370
Manuel Ortega <maopmore@gmail.com>
2024-04-21 19:47:25 +0200

    Added next-pwa package and configured PWA settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const withPWA = require('next-pwa');
+
+const nextConfig = withPWA({
+    pwa: {
+        dest: 'public',
+    },
+});
 
 module.exports = nextConfig;
