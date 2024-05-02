ab63520a523267e448743893fc5d216bd7836914
Manuel Ortega <maopmore@gmail.com>
2024-05-02 13:47:15 +0200

    Refactor quiz page layout and add scroll area component

--- app/(main)/quiz/page.tsx
+++ app/(main)/quiz/page.tsx
@@ -2,11 +2,12 @@ import { Progress } from '@/components/ui/progress';
 import { AvatarImage, AvatarFallback, Avatar } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Heart } from 'lucide-react';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
-export default function Component() {
+export default function Quiz() {
   return (
-    <div className=" p-8">
-      <div className="max-w-3xl mx-auto">
+    <div className="p-8 h-full max-h-full">
+      <div className="max-w-3xl mx-auto max-h-full">
         <div className="flex justify-between items-center mb-6 gap-4">
           <Progress className="w-full" value={90} />
           <div className="flex items-center space-x-2">
@@ -19,32 +20,46 @@ export default function Component() {
           </Avatar> */}
         </div>
         <div className="mb-6 p-6 border rounded-md shadow ">
-          <h2 className="text-lg font-semibold text-center">
-            What is the capital of France? Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Saepe iusto ducimus velit fuga esse,
-            assumenda omnis voluptatem soluta neque impedit optio molestiae
-            dolores deleniti illum praesentium aspernatur molestias sit. Laborum
-            dolor commodi omnis et, ab minus possimus ea nesciunt quibusdam
-            earum qui quisquam sit dolorum accusamus! Inventore nesciunt amet
-            ex!
+          <h2 className="zsm:text-sm font-semibold text-center">
+            What is the capital of France? Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Labore non placeat, quod aliquid
+            ratione quae fugit distinctio tempora suscipit, voluptates
+            voluptatem, dolorum voluptatum. Quisquam, quod. Lorem ipsum dolor sit
           </h2>
         </div>
-        <div className="grid gap-4">
-          <Button variant="success" className="w-full py-4">
-            Paris - Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        <ScrollArea>
+          <div className="grid grid-cols-1 gap-4  h-full auto-rows-[1fr] ">
+            <Button
+              variant="success"
+              className="py-4 !h-auto whitespace-normal"
+            >
+              Paris - Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </Button>
+            <Button
+              variant="destructive"
+              className="py-4 !h-auto whitespace-normal"
+            >
+              London - Lorem ipsum dolor sit amet consectetur adipisicing
+              elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </Button>
+            <Button
+              variant="secondary"
+              className="py-4 !h-auto whitespace-normal"
+            >
+              Berlin - Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </Button>
+            <Button
+              variant="secondary"
+              className="py-4 !h-auto whitespace-normal"
+            >
+              Madrid -
+            </Button>
+          </div>
+        </ScrollArea>
+        <div className="flex flex-row-reverse">
+          <Button variant="default" className="mt-6 w-1/3 py-4 ">
+            Next
           </Button>
-          <Button variant="destructive" className="w-full py-4">
-            London - Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </Button>
-          <Button variant="secondary" className="w-full py-4">
-            Berlin - Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </Button>
-          <Button variant="secondary" className="w-full py-4">
-            Madrid - Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </Button>
-        </div>
-        <div className='flex flex-row-reverse'>
-          <Button variant="default" className="mt-6 w-1/3 py-4 ">Next</Button>
         </div>
       </div>
     </div>
--- components/SidebarSubjects.tsx
+++ components/SidebarSubjects.tsx
@@ -6,7 +6,7 @@ import { Subject } from '@/schemas/subjects'; // Import the Subject type
 export default async function SidebarSubjects() {
   const subjects: Subject[] = await allSubjects(); // Specify the type of the subjects variable
   return (
-    <div className="flex-1 border-r pt-2">
+    <div className="flex-1 md:border-r pt-2">
       <div className="px-3 py-2">
         <div className="flex items-center pl-4 justify-between mb-2">
           <h3 className="font-semibold text-lg tracking-tight">Subjects</h3>
