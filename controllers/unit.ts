a220a15aa91d9463feb36d68ba417f7d302f55ae
Dani <dani96sk@hotmail.com>
2024-05-02 10:44:41 +0200

    Created getUnitsFromSubject endpoints

--- controllers/unit.ts
+++ controllers/unit.ts
@@ -1,10 +1,12 @@
 "use server";
 import { InsertUnit, units } from "@/schemas/units";
 import { db } from "@/utils/drizzle/db";
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 
-export const addunit = async (unit: InsertUnit) => {
-  await db.insert(units).values(unit);
+export const addUnit = async (unit: InsertUnit) => {
+  await db
+  .insert(units)
+  .values(unit);
 };
 
 export const allUnits = async () => {
@@ -13,7 +15,9 @@ export const allUnits = async () => {
 };
 
 export const deleteUnit = async (id: number) => {
-  await db.delete(units).where(eq(units.id, id));
+  await db
+    .delete(units)
+    .where(eq(units.id, id));
 };
 
 export const updateUnit = async (id: number, unit: InsertUnit) => {
@@ -25,3 +29,24 @@ export const updateUnit = async (id: number, unit: InsertUnit) => {
     })
     .where(eq(units.id, id));
 };
+
+export const getUnitsFromSubject= async (subjectId: number) => {
+  const data = await db
+    .select()
+    .from(units)
+    .where(eq(units.subjectId, subjectId));
+  return data;
+}
+
+export const getActiveUnitsFromSubject = async (subjectId: number) => {
+  const data = await db
+    .select()
+    .from(units)
+    .where(
+      and(
+        eq(units.subjectId, subjectId), 
+        eq(units.active, true)
+      )
+    )
+  return data;
+}
