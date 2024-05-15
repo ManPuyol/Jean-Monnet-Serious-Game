1808983e004416438a317be95daa42ed32d9516a
Manuel Ortega <maopmore@gmail.com>
2024-05-15 17:51:56 +0200

    reditect to login on root route

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,23 @@ import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
+  // const canInitSupabaseClient = () => {
+  //   // This function is just for the interactive tutorial.
+  //   // Feel free to remove it once you have Supabase connected.
+  //   try {
+  //     createClient();
+  //     return true;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // };
 
-  const isSupabaseConnected = canInitSupabaseClient();
+  // const isSupabaseConnected = canInitSupabaseClient();
+
+  redirect("/sign-in");
 
   return (
     <>
