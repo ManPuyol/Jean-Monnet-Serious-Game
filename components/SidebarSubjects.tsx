4ed45546a3e4e19dda118bf9dbb02d40eb4fad3f
Manuel Ortega <maopmore@gmail.com>
2024-05-20 20:55:25 +0200

    fixed teach sidebar styles

--- components/SidebarSubjects.tsx
+++ components/SidebarSubjects.tsx
@@ -1,18 +1,27 @@
 import { allSubjects } from '@/controllers/subjects';
-import  AddSubject  from '@/app/(main)/teach/create-subject';
+import AddSubject from '@/app/(main)/teach/create-subject';
 import SubjectNavigation from './SubjectNavigation';
 import { Subject } from '@/schemas/subjects'; // Import the Subject type
 
-export default async function SidebarSubjects({activeSubjectId}: {activeSubjectId: string}) {
+export default async function SidebarSubjects({
+  activeSubjectId,
+}: {
+  activeSubjectId: string;
+}) {
   const subjects: Subject[] = await allSubjects();
+  console.log(activeSubjectId);
   return (
-    <div className="f lex-1 md:border-r pt-2">
+    <div className="flex-1 md:border-r pt-2">
       <div className="px-3 py-2">
         <div className="flex items-center pl-4 justify-between mb-2">
           <h3 className="font-semibold text-lg tracking-tight">Subjects</h3>
           <AddSubject />
         </div>
-        <SubjectNavigation subjects={subjects} path='teach' activeSubjectId={activeSubjectId} />
+        <SubjectNavigation
+          subjects={subjects}
+          path="teach"
+          activeSubjectId={activeSubjectId}
+        />
       </div>
     </div>
   );
