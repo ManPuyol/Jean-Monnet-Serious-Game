8535c7466b604385656d358a26c8b3a38c18647c
Manuel Ortega <maopmore@gmail.com>
2024-05-21 17:42:48 +0200

    Using path to highlight active subject

--- components/SidebarSubjects.tsx
+++ components/SidebarSubjects.tsx
@@ -3,13 +3,8 @@ import AddSubject from '@/app/(main)/teach/create-subject';
 import SubjectNavigation from './SubjectNavigation';
 import { Subject } from '@/schemas/subjects'; // Import the Subject type
 
-export default async function SidebarSubjects({
-  activeSubjectId,
-}: {
-  activeSubjectId: string;
-}) {
+export default async function SidebarSubjects() {
   const subjects: Subject[] = await allSubjects();
-  console.log(activeSubjectId);
   return (
     <div className="flex-1 md:border-r pt-2">
       <div className="px-3 py-2">
@@ -17,11 +12,7 @@ export default async function SidebarSubjects({
           <h3 className="font-semibold text-lg tracking-tight">Subjects</h3>
           <AddSubject />
         </div>
-        <SubjectNavigation
-          subjects={subjects}
-          path="teach"
-          activeSubjectId={activeSubjectId}
-        />
+        <SubjectNavigation subjects={subjects} path="teach" />
       </div>
     </div>
   );
