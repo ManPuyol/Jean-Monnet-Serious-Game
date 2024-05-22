278ebd08f7d50045f13ddf1b49ac820787db4b1e
Manuel Ortega <maopmore@gmail.com>
2024-05-22 23:22:28 +0200

    Added page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Jean Monnet",
+  description: "Jean Monnet serious game for learning European Union history and institutions",
 };
 
 export default function RootLayout({
