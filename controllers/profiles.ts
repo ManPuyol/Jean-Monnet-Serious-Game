5c54c548c70957397b9f1f49617777c62965058a
Dani <dani96sk@hotmail.com>
2024-05-25 13:00:06 +0200

    Added controller to retrieve data from user progress
    also added the view schema

--- controllers/profiles.ts
+++ controllers/profiles.ts
@@ -1,6 +1,9 @@
 "use server"
 
+import { viewCounterAchievements } from "@/interfaces/viewCounterAchievements";
 import { getUser } from "@/lib/utils";
+import { db } from "@/utils/drizzle/db";
+import { eq, sql } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export const getDataForProfile = async () => {
@@ -19,4 +22,25 @@ export const getDataForProfile = async () => {
     return {
         nameInitials: fullNameInitials,
     }
+}
+
+export const getDataForAchievements = async () => {
+    const user = await getUser();
+
+    if (!user){
+        NextResponse.json("Unable to find user");
+    }
+
+    const data = await db
+    .select({
+        quizzesDone : viewCounterAchievements.quizzesDone,
+        quizzesPassed : viewCounterAchievements.quizzesPassed,
+        quizzesPerfect : viewCounterAchievements.quizzesPerfect,
+    })
+    .from(viewCounterAchievements)
+    .where(
+        eq(viewCounterAchievements.userId, user!.id)
+    )
+
+    return data;
 }
